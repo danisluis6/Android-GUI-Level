ec9bb3736da9e495edb78effc27b6c78549881c2
danisluis6 <vuongluis@hotmail.com>
2016-12-06 17:20:44 +0700

    ok

--- LoginApp/app/src/main/java/com/example/enclaveit/loginapp/MainActivity.java
+++ LoginApp/app/src/main/java/com/example/enclaveit/loginapp/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.enclaveit.loginapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /**
+         * With Login Acitivity.
+         * You need to set the background images and an important things is that you need to set FullScreen mode.
+         */
+        // Set FullScreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Set No Title
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
     }
 }
